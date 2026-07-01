0b4c1a1a3f33995b8bfb2734a6348d8e1c06b231
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-06-30 21:17:03 -0700

    Add many setters for many properties to ExplosionRoot

--- Source/Environment/Explosions/ExplosionRoot.cs
+++ Source/Environment/Explosions/ExplosionRoot.cs
@@ -19,6 +19,24 @@ namespace Nyxpiri.ULTRAKILL.NyxLib
             }
         }
 
+        public bool FriendlyFire { set => ForEachExplosion((e) => { e.friendlyFire = value; }); }
+        public bool Enemy { set => ForEachExplosion((e) => { e.enemy = value; }); }
+        public bool IsFup { set => ForEachExplosion((e) => { e.isFup = value; }); }
+        public bool Ultrabooster { set => ForEachExplosion((e) => { e.ultrabooster = value; }); }
+        public bool RocketExplosion { set => ForEachExplosion((e) => { e.rocketExplosion = value; }); }
+
+        public bool Ignite { set => ForEachExplosion((e) => { e.enemy = value; }); }
+        public bool Eletric { set => ForEachExplosion((e) => { e.electric = value; }); }
+
+        public bool Unblockable { set => ForEachExplosion((e) => { e.unblockable = value; }); }
+        public bool Halved { set => ForEachExplosion((e) => { e.halved = value; }); }
+
+        public string HitterWeapon { set => ForEachExplosion((e) => { e.hitterWeapon = value; }); }
+        public EnemyIdentifier OriginEid { set => ForEachExplosion((e) => { e.originEnemy = value; }); }
+
+        public AffectedSubjects CanHit { set => ForEachExplosion((e) => { e.canHit = value; }); }
+        public List<EnemyType> ToIgnore { set => ForEachExplosion((e) => { e.toIgnore = value; }); }
+
         public void SetMaxDamage(int maxDamage)
         {
             if (Explosions.Count == 0)
@@ -36,6 +54,51 @@ namespace Nyxpiri.ULTRAKILL.NyxLib
             float scalar = maxDamage / highestDamage;
 
             ScaleDamage(scalar);
+        }
+
+        public void SetMaxSize(float maxSize)
+        {
+            if (Explosions.Count == 0)
+            {
+                return;
+            }
+
+            float highestVal = float.NegativeInfinity;
+
+            foreach (var explosion in Explosions)
+            {
+                highestVal = Math.Max(explosion.maxSize, highestVal);
+            }
+
+            float scalar = maxSize / highestVal;
+
+            ScaleSize(scalar);
+        }
+
+        public void SetMaxSpeed(float maxSpeed)
+        {
+            if (Explosions.Count == 0)
+            {
+                return;
+            }
+
+            float highestVal = GetMaxSpeed();
+
+            float scalar = maxSpeed / highestVal;
+
+            ScaleSpeed(scalar);
+        }
+
+        public float GetMaxSpeed()
+        {
+            float highestVal = float.NegativeInfinity;
+
+            foreach (var explosion in Explosions)
+            {
+                highestVal = Math.Max(explosion.speed, highestVal);
+            }
+
+            return highestVal;
         }
 
         public void SetMaxEnemyDamageMultiplier(float maxVal)
@@ -109,11 +172,6 @@ namespace Nyxpiri.ULTRAKILL.NyxLib
         public void MakeHarmless()
         {
             ForEachExplosion((e) => { e.harmless = true; });
-        }
-
-        public void SetFriendlyFire(bool friendlyFire)
-        {
-            ForEachExplosion((e) => { e.friendlyFire = friendlyFire; });
         }
 
         public void ScalePushForce(float scalar)
