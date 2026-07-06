7b218c109f75633b1d67c74a8545b4806fc6d381
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-07-06 10:53:33 -0700

    add shaker3D

--- Source/math/Shaker.cs
+++ Source/math/Shaker.cs
@@ -54,7 +54,7 @@ namespace NyxpiriOS // made for my godot game, yoinked it and converted it to th
             get
             {
                 float processedAlpha = Alpha;
-                
+
                 processedAlpha = Mathf.Sin((processedAlpha + 0.5f) * ((float)Math.PI));
 
                 if (Alpha < 0.5f)
@@ -77,17 +77,128 @@ namespace NyxpiriOS // made for my godot game, yoinked it and converted it to th
             for (int i = 0; (i < 10 && Vector3.Distance(PositionB, PositionA) < MinDistance) || i < 1; i++)
             {
                 Vector2 randomUnitVec2 = UnityEngine.Random.insideUnitCircle.normalized;
-                
+
                 randomUnitVec2 = randomUnitVec2 == Vector2.zero ? Vector2.right : randomUnitVec2;
                 PositionB = randomUnitVec2 * UnityEngine.Random.Range(MinScale, MaxScale);
 
                 if (Vector2.Distance(PositionB, PositionA) < MinDistance)
                 {
-                    PositionB +=  (PositionB - PositionA).normalized * (MinDistance - Vector2.Distance(PositionB, PositionA));
+                    PositionB += (PositionB - PositionA).normalized * (MinDistance - Vector2.Distance(PositionB, PositionA));
 
                     if (PositionB.magnitude > MaxScale || PositionB.magnitude < MinScale)
                     {
                         PositionB = Vector2.zero;
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void Process(float delta)
+        {
+            Alpha += (delta * (Rate));
+
+            if (Alpha > 1.0f)
+            {
+                Alpha %= 1.0f;
+                NextPosition();
+            }
+        }
+    }
+
+
+    [Serializable]
+    public class Shaker3D
+    {
+        [SerializeField] private float _MinScale = 0.0f;
+        public float MinScale
+        {
+            get => _MinScale;
+            set
+            {
+                _MinScale = value;
+            }
+        }
+
+        [SerializeField] private float _MaxScale = 1.0f;
+        public float MaxScale
+        {
+            get => _MaxScale;
+            set
+            {
+                _MaxScale = value;
+            }
+        }
+
+        [SerializeField] private float _MinDistance = 1.0f;
+        public float MinDistance
+        {
+            get => _MinDistance;
+            set
+            {
+                _MinDistance = value;
+            }
+        }
+
+        [SerializeField] private float _Rate = 50.0f;
+        public float Rate
+        {
+            get => _Rate;
+            set
+            {
+                _Rate = Mathf.Max(0.0f, value);
+            }
+        }
+
+        [SerializeField] private Vector3 _positionA = Vector3.zero;
+        public Vector3 PositionA { get => _positionA; private set => _positionA = value; }
+
+        [SerializeField] private Vector3 _positionB = Vector3.zero;
+        public Vector3 PositionB { get => _positionB; private set => _positionB = value; }
+
+        [SerializeField] private float _alpha = 0.0f;
+        public float Alpha { get => _alpha; private set => _alpha = value; }
+
+        public Vector3 Position
+        {
+            get
+            {
+                float processedAlpha = Alpha;
+
+                processedAlpha = Mathf.Sin((processedAlpha + 0.5f) * ((float)Math.PI));
+
+                if (Alpha < 0.5f)
+                {
+                    processedAlpha = (1.0f - processedAlpha) * 0.5f;
+                }
+                else
+                {
+                    processedAlpha = 0.5f + (Mathf.Abs(processedAlpha) * 0.5f);
+                }
+
+                return Vector3.Lerp(PositionA, PositionB, processedAlpha);
+            }
+        }
+
+        public void NextPosition()
+        {
+            PositionA = PositionB;
+
+            for (int i = 0; (i < 10 && Vector3.Distance(PositionB, PositionA) < MinDistance) || i < 1; i++)
+            {
+                PositionB = UnityEngine.Random.insideUnitSphere * UnityEngine.Random.Range(MinScale, MaxScale);
+
+                if (Vector3.Distance(PositionB, PositionA) < MinDistance)
+                {
+                    PositionB += (PositionA - PositionB).normalized * (MinDistance - Vector3.Distance(PositionB, PositionA));
+
+                    if (PositionB.magnitude > MaxScale || PositionB.magnitude < MinScale)
+                    {
+                        PositionB = Vector3.zero;
                         continue;
                     }
                     else
