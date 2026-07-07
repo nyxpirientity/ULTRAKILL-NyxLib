c91b5c59c20dc85885af52fecf1bf3109dd6a985
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-07-06 22:43:17 -0700

    add DebugPrintMaterialProperties

---
+++ Source/Diagnostics/Debugging/AssetInspecting.cs
@@ -0,0 +1,20 @@
+using System;
+using Nyxpiri.ULTRAKILL.NyxLib.Diagnostics.Debug;
+using UnityEngine;
+
+namespace Nyxpiri.ULTRAKILL.NyxLib
+{
+    public static class AssetInspecting
+    {
+        public static void DebugPrintMaterialProperties(this Material material)
+        {
+            foreach (var type in Enum.GetValues(typeof(MaterialPropertyType)))
+            {
+                foreach (var name in material.GetPropertyNames((MaterialPropertyType)type))
+                {
+                    Log.Message($"Material Debug! There's a {type} named {name}");
+                }
+            }
+        }
+    }
+}
